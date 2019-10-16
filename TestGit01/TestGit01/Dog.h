08b8602d80aa570cab4f6be38a5a61d4575c55ce
949699582 <zhouhongqiang@intelligencechain.io>
2019-10-16 16:11:16 +0800

    提交dog类

---
+++ TestGit01/TestGit01/Dog.h
@@ -0,0 +1,17 @@
+//
+//  Dog.h
+//  TestGit01
+//
+//  Created by 949699582 on 2019/10/16.
+//  Copyright © 2019 com.cunyin.paywaiter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Dog : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestGit01/TestGit01/Dog.m
@@ -0,0 +1,13 @@
+//
+//  Dog.m
+//  TestGit01
+//
+//  Created by 949699582 on 2019/10/16.
+//  Copyright © 2019 com.cunyin.paywaiter. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
