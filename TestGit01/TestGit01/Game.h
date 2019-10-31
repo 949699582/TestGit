1d1737643e157c0898622f261b7ec114e73a866d
949699582 <zhouhongqiang@intelligencechain.io>
2019-10-31 11:14:51 +0800

    测试git多人提交bug

---
+++ TestGit01/TestGit01/Game.h
@@ -0,0 +1,17 @@
+//
+//  Game.h
+//  TestGit01
+//
+//  Created by 949699582 on 2019/10/31.
+//  Copyright © 2019 com.cunyin.paywaiter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Game : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestGit01/TestGit01/Game.m
@@ -0,0 +1,13 @@
+//
+//  Game.m
+//  TestGit01
+//
+//  Created by 949699582 on 2019/10/31.
+//  Copyright © 2019 com.cunyin.paywaiter. All rights reserved.
+//
+
+#import "Game.h"
+
+@implementation Game
+
+@end
