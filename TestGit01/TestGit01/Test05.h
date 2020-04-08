0ebafc72605ba40fdf489e5c25e9e0ed7c80c609
949699582 <zhouhongqiang@intelligencechain.io>
2020-04-08 13:51:07 +0800

    TEST05

---
+++ TestGit01/TestGit01/Test05.h
@@ -0,0 +1,17 @@
+//
+//  Test05.h
+//  TestGit01
+//
+//  Created by 949699582 on 2020/4/8.
+//  Copyright © 2020 com.cunyin.paywaiter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Test05 : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestGit01/TestGit01/Test05.m
@@ -0,0 +1,13 @@
+//
+//  Test05.m
+//  TestGit01
+//
+//  Created by 949699582 on 2020/4/8.
+//  Copyright © 2020 com.cunyin.paywaiter. All rights reserved.
+//
+
+#import "Test05.h"
+
+@implementation Test05
+
+@end
