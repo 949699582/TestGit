6233dc1ea19af08b7b981132481eb1ae6d4fdda1
949699582 <zhouhongqiang@intelligencechain.io>
2020-04-08 13:40:33 +0800

    Test06

---
+++ TestGit01/TestGit01/Test06.m
@@ -0,0 +1,13 @@
+//
+//  Test06.m
+//  TestGit01
+//
+//  Created by 949699582 on 2020/4/8.
+//  Copyright © 2020 com.cunyin.paywaiter. All rights reserved.
+//
+
+#import "Test06.h"
+
+@implementation Test06
+
+@end
